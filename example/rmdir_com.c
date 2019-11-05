0aff9bb3b37f7b5162149a32f1653edb45f25b05
SILKYMAJOR <silky.major@gmail.com>
2019-11-05 13:14:01 +0900

    Add rmdir_com

---
+++ example/rmdir_com.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+#include <stdio.h>
+
+int main(int argc, char **argv){
+  int i;
+  int err = 0;
+  if(argc <= 1){
+    fprintf(stderr, "Usage: %s file...\n", argv[0]);
+    return 1;
+  }
+  for(i=1;i<argc;i++){
+    if(rmdir(argv[i]) < 0){
+      perror(argv[i]);
+      err = 1;
+    }
+  }
+  return err;
+}
